0d7872d33116ea9a79f5cb5838180f07af25798d
L4Limbo <anestis2799@gmail.com>
2022-01-13 01:48:57 +0200

    New Migrations and Seeders

--- data_generation/data_generation.py
+++ data_generation/data_generation.py
@@ -1,45 +1,82 @@
 import faker
 import random
 import pandas as pd 
+import string
 
 fake = faker.Faker('en_GB');
 
 
-def fake_passengers(count=10):
-    df = pd.DataFrame(columns=['fname','lname','country_code','phone_number','email','nationality','birthdate'])
-    for i in range(0, count):
-        data = {'fname': fake.name().split()[0],
-                'lname': fake.name().split()[1],
-                'country_code': fake.country_code(),
-                'phone_number': fake.phone_number(),
-                'email': fake.email(),
-                'nationality': fake.country(),
-                'birthdate': fake.date_of_birth()}
-        df = df.append(data, ignore_index=True) 
-    df.to_csv('data_generation/fake_passengers.csv')
+def ports_generation():
+    pass
+    # https://ec.europa.eu/eurostat/cache/metadata/Annexes/mar_esms_an2.xlsx
+    # df = pd.read_csv('data_generation/ports.csv',encoding='cp1252')
+    # df2 = df[['CTRY', 'Country/Port Name']]
+    # df2 = df2[df2.CTRY == 'EL']
+    # df2.to_csv('data_generation/el_ports.csv')
     
-
-def fake_coupons(count=10):
-    df = pd.DataFrame(columns=['code','used'])
-    for i in range(0, count):
-        data = {'code': fake.word().upper(),
-                'used': random.randint(0, 1)}
-        df = df.append(data, ignore_index=True) 
-    df.to_csv('data_generation/fake_coupons.csv')
-
-
-def fake_tickets(count=10):
-    df = pd.DataFrame(columns=['ticket_code','category','vehicle'])
-    categories = ['Adult', 'Infant', 'Child', 'Student']
-    vehicles = ['Car','Bicyle','Truck','Van']
-    for i in range(0, count):
-        data = {'ticket_code': fake.ean(8),
-                'category': random.choice(categories),
-                'vehicle': 'null'}
-        data['vehicle'] = random.choice(vehicles) if(data['category']=='Adult') else 'null'
-        df = df.append(data, ignore_index=True)
-    df.to_csv('data_generation/fake_tickets.csv')
-
+def vehicle_type_generation():
+    df = pd.DataFrame(columns=['type','description','fee'])
+    data = [
+       {
+            'type': 'Car',
+            'description': '(Vehicles up to 5m long and 2m high)',
+            'fee': 1.5
+       },
+       {
+            'type': 'Motorcycle',
+            'description': '(All motorcycles)',
+            'fee': 0.5
+       },
+       {
+            'type': 'Truck',
+            'description': 'Vehicles up to 8m long and 3.5m high',
+            'fee': 2.0
+       }
+    ]
+    df = df.append(data, ignore_index=True)
+    df.to_csv('data_generation/vehicle_types.csv')
+    
+def ticket_type_generation():
+    df = pd.DataFrame(columns=['type','description','discount'])
+    data = [
+       {
+            'type': 'Adult',
+            'description': 'null',
+            'discount': 0.0
+       },
+       {
+            'type': 'Infant',
+            'description': '(Ages: 0 - 5)',
+            'discount': 1.0
+       },
+       {
+            'type': 'Child',
+            'description': '(Ages: 5 - 16)',
+            'discount': 0.25
+       },
+       {
+            'type': 'Student',
+            'description': 'null',
+            'discount': 0.5
+       },
+       {
+            'type': 'Senior',
+            'description': 'null',
+            'discount': 0.5
+       },
+       {
+            'type': 'Passenger with reduced mobility',
+            'description': 'null',
+            'discount': 0.25
+       },
+       {
+            'type': 'Greek military personel',
+            'description': 'null',
+            'discount': 1.0
+       },
+    ]
+    df = df.append(data, ignore_index=True)
+    df.to_csv('data_generation/ticket_types.csv')
 
 def fake_companies(count=3):
     df = pd.DataFrame(columns=['name','api_url','api_psw'])
@@ -51,37 +88,13 @@ def fake_companies(count=3):
         df = df.append(data, ignore_index=True)
     df.to_csv('data_generation/fake_companies.csv')
 
-
-def fake_ships(count=20):
-    df = pd.DataFrame(columns=['name','availability','fullness','type'])
-    ship_types = ['high-speed', 'passenger', 'roll-on-roll-off', ]
+def fake_coupons(count=10):
+    df = pd.DataFrame(columns=['code','discount'])
     for i in range(0, count):
-        data = {'name': fake.name().split()[0],
-                'availability': random.randint(300, 1000),
-                'fullness': random.randint(100, 300),
-                'type': random.choice(ship_types)}
-        df = df.append(data, ignore_index=True)
-    df.to_csv('data_generation/fake_ships.csv')
-    
-    
-def ports_generation(count=10):
-    pass
-    # https://ec.europa.eu/eurostat/cache/metadata/Annexes/mar_esms_an2.xlsx
-    # df = pd.read_csv('data_generation/ports.csv',encoding='cp1252')
-    # df2 = df[['CTRY', 'Country/Port Name']]
-    # df2 = df2[df2.CTRY == 'EL']
-    # df2.to_csv('data_generation/el_ports.csv')
-
-
-def fake_services(count=0):
-    df = pd.DataFrame(columns=['name'])
-    services = ['restaurant', 'cabins', 'vehicles', 'wifi', 'etickets']
-    for i in range(0, len(services)):
-        data = {'name': services[i]}
-        
-        df = df.append(data, ignore_index=True)
-    df.to_csv('data_generation/fake_services.csv')
-    
+        data = {'code': fake.word().upper(),
+                'discount': random.choice([0.1, 0.15, 0.25])}
+        df = df.append(data, ignore_index=True) 
+    df.to_csv('data_generation/fake_coupons.csv')
     
 def fake_payments(count=20):
     df = pd.DataFrame(columns=['insurance','sms', 'payment_date', 'payment_method'])
@@ -94,33 +107,53 @@ def fake_payments(count=20):
                 }
         df = df.append(data, ignore_index=True)
     df.to_csv('data_generation/fake_payments.csv')
-    
 
-def fake_special_seats(count=40):
-    df = pd.DataFrame(columns=['insurance','sms', 'payment_date', 'payment_method'])
-    types = ['deck', 'cabin', 'vip', 'airplane']
+def fake_passengers(count=10):
+    df = pd.DataFrame(columns=['fname','lname','country_code','phone_number','email','birthdate','id_card'])
     for i in range(0, count):
-        data = {'type': random.choice(types),
-                'availability': random.randint(0, 100),       
-                'description': fake.sentence(), 
-                'no_seat': "No" + str(random.randint(1, 50)),
-                'no_bed': random.randint(1, 6)
-                }
+        data = {'fname': fake.name().split()[0],
+                'lname': fake.name().split()[1],
+                'country_code': fake.country_code(),
+                'phone_number': fake.phone_number(),
+                'email': fake.email(),
+                'birthdate': fake.date_of_birth(),
+                'id_card': 2*random.choice(string.ascii_letters).upper() + fake.ean(8)}
+        df = df.append(data, ignore_index=True) 
+    df.to_csv('data_generation/fake_passengers.csv')
+
+def fake_tickets(count=10):
+    df = pd.DataFrame(columns=['ticket_code','t_type','v_type'])
+    categories = ['Adult', 'Infant', 'Child', 'Student','Greek military personel','Passenger with reduced mobility','Senior']
+    vehicles = ['Car','Bicyle','Truck','Van']
+    for i in range(0, count):
+        data = {'ticket_code': fake.ean(8),
+                'special_seat': 'null',
+                't_type': random.choice(categories),
+                'v_type': 'null'}
+        data['v_type'] = random.choice(vehicles) if(data['t_type']!='Child' or data['t_type']!='Infant') else 'null'
         df = df.append(data, ignore_index=True)
-    df.to_csv('data_generation/fake_special_seats.csv')
-    
+    df.to_csv('data_generation/fake_tickets.csv')
 
+def fake_ships(count=20):
+    df = pd.DataFrame(columns=['name', 'type'])
+    ship_types = ['high-speed', 'passenger', 'roll-on-roll-off', ]
+    for i in range(0, count):
+        data = {'name': fake.name().split()[0],
+                'type': random.choice(ship_types)}
+        df = df.append(data, ignore_index=True)
+    df.to_csv('data_generation/fake_ships.csv')
+    
 def fake_generation(count=10):
-    fake_coupons(count)
-    fake_passengers(count)
-    fake_tickets(count)
-    fake_companies()
-    fake_ships()
-    fake_services()
-    fake_payments()
-    fake_special_seats()
+    # ports_generation()
+    vehicle_type_generation()
+    ticket_type_generation()
+    fake_companies(3)
+    fake_coupons(10)
+    fake_payments(20)
+    fake_passengers(20)
+    fake_tickets(10)
+    fake_ships(20)
     
-
 def main():
     print('Generator')
     
--- src/migrate.py
+++ src/migrate.py
@@ -1,5 +1,4 @@
 import db
-
 dbfile = "db/ferry.db"
 db = db.DataModel(dbfile)
 
@@ -7,126 +6,129 @@ db = db.DataModel(dbfile)
 def migrate_tables():
     db.executeSQL(
         ''' 
-            CREATE TABLE HARBOR (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                area varchar(32) DEFAULT NULL,
-                name varchar(32) NOT NULL
-            );
-            
-            CREATE TABLE COMPANY (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                name varchar(32),
-                api_url varchar(64) DEFAULT NULL,
-                api_psw varchar(32) DEFAULT NULL
-            );
-            
-            CREATE TABLE PASSENGER (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                fname varchar(32),
-                lname varchar(32),
-                country_code integer,
-                phone_number varchar(32),
-                email varchar(64),
-                nationality varchar(64),
-                birthdate datetime
-            );
-            
-            CREATE TABLE COUPON (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                code varchar(10),
-                used binary
-            );
-            
-            CREATE TABLE SERVICE (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                name varchar(32)
-            );
-            
-            CREATE TABLE SHIP (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                name varchar(32) NOT NULL,
-                type varchar(32) NOT NULL,
-                availability integer DEFAULT NULL,
-                fullness integer DEFAULT NULL,
-                service_id integer,
-                company_id integer,
-                FOREIGN KEY (service_id) REFERENCES SERVICE(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (company_id) REFERENCES COMPANY(id) ON DELETE CASCADE ON UPDATE CASCADE
-            );
-            
-            CREATE TABLE SPECIAL_SEAT (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                type varchar (32),
-                availability binary,
-                description varchar(128),
-                no_seat varchar(8),
-                no_bed integer DEFAULT NULL,
-                ship_id binary DEFAULT NULL,
-                FOREIGN KEY (ship_id) REFERENCES SHIP(id) ON DELETE CASCADE ON UPDATE CASCADE
-            );
-            
-            CREATE TABLE ROUTE (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                description varhar(128),
-                company_id integer,
-                ship_id integer,
-                FOREIGN KEY (ship_id) REFERENCES SHIP(id) ON DELETE CASCADE ON UPDATE CASCADE
-                FOREIGN KEY (company_id) REFERENCES COMPANY(id) ON DELETE CASCADE ON UPDATE CASCADE
+            CREATE TABLE PORT (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                area VARCHAR(64) DEFAULT NULL,
+                name VARCHAR(64) NOT NULL
             );
 
-            CREATE TABLE STATION (
-                route_id integer,
-                harbor_id integer,
-                arrival_date datetime,
-                cost float,
-                FOREIGN KEY (route_id) REFERENCES ROUTE(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (harbor_id) REFERENCES HARBOR(id) ON DELETE CASCADE ON UPDATE CASCADE
-            );   
-            
-            CREATE TABLE TICKET (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                ticket_code varchar(32) NOT NULL,
-                cost float,
-                category varchar(32),
-                vehicle varchar(16),
-                payment_id integer,
-                passenger_id integer,
-                special_seat_id integer ,
-                route_id integer,
-                FOREIGN KEY (payment_id) REFERENCES PAYMENT(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (passenger_id) REFERENCES PASSENGER(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (special_seat_id) REFERENCES SPECIAL_SEAT(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (special_seat_id) REFERENCES ROUTE(id) ON DELETE CASCADE ON UPDATE CASCADE
+            CREATE TABLE VEHICLE_TYPE (
+                type VARCHAR(64) PRIMARY KEY,
+                fee FLOAT DEFAULT NULL,
+                description VARCHAR(255) DEFAULT NULL
             );
+
+            CREATE TABLE TICKET_TYPE (
+                type VARCHAR(64) PRIMARY KEY,
+                discount DECIMAL(3,2) DEFAULT NULL,
+                description VARCHAR(255) DEFAULT NULL
+            );
+
+            CREATE TABLE COMPANY (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(32),
+                api_url VARCHAR(64) DEFAULT NULL,
+                api_psw VARCHAR(32) DEFAULT NULL
+            );
+
+            CREATE TABLE COUPON (
+                code VARCHAR(8) PRIMARY KEY,
+                discount DECIMAL(3,2) DEFAULT NULL
+            ); 
 
             CREATE TABLE PAYMENT (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                insurance binary,
-                sms binary,
-                payment_date datetime,
-                total_Cost float,
-                payment_method varchar(16),
-                coupon_id integer,
-                FOREIGN KEY (coupon_id) REFERENCES COUPON(id) ON DELETE CASCADE ON UPDATE CASCADE
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                insurance BINARY,
+                sms BINARY,
+                payment_date TEXT,
+                total_cost FLOAT,
+                payment_method VARCHAR(16),
+                coupon_code VARCHAR(8) DEFAULT NULL,
+                FOREIGN KEY (coupon_code) REFERENCES COUPON(code) ON DELETE SET DEFAULT ON UPDATE SET DEFAULT
+            );
+
+            CREATE TABLE PASSENGER (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                fname VARCHAR(32),
+                lname VARCHAR(32),
+                country_code VARCHAR(3),
+                phone_number VARCHAR(32),
+                email VARCHAR(64),
+                birthdate TEXT,
+                id_card VARCHAR(16) UNIQUE
+            );
+
+            CREATE TABLE TRIP (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ship_name VARCHAR(64),
+                ship_type VARCHAR(64),
+                car_cap INTEGER,
+                deck_cap INTEGER,
+                air_cap INTEGER,
+                dcab_cap INTEGER,
+                qcab_cap INTEGER,
+                company_id INTEGER DEFAULT NULL,
+                
+                FOREIGN KEY (company_id) REFERENCES COMPANY(id) ON DELETE CASCADE ON UPDATE CASCADE
+            );
+
+            CREATE TABLE ROUTE(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                cost FLOAT,
+                deck_cap INTEGER,
+                dep_date TEXT,
+                arr_date TEXT,
+                route_seq INTEGER,
+                dep_port_id INTEGER,
+                arr_port_id INTEGER,
+                trip_id INTEGER,
+                
+                
+                FOREIGN KEY (trip_id) REFERENCES TRIP(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (dep_port_id) REFERENCES PORT(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (arr_port_id) REFERENCES PORT(id) ON DELETE CASCADE ON UPDATE CASCADE
+            );
+
+            CREATE TABLE TICKET (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ticket_code VARCHAR(64) UNIQUE,
+                cost FLOAT,
+                special_seat VARCHAR(64),
+                v_type VARCHAR(64),
+                t_type VARCHAR(64),
+                payment_id INTEGER,
+                passenger_id INTEGER,
+                trip_id INTEGER,
+                
+                FOREIGN KEY (v_type) REFERENCES VEHICLE_TYPE(type) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (t_type) REFERENCES TICKET_TYPE(type) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (payment_id) REFERENCES PAYMENT(id) ON DELETE SET NULL ON UPDATE CASCADE,
+                FOREIGN KEY (passenger_id) REFERENCES PASSENGER(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (trip_id) REFERENCES TRIP(id) ON DELETE CASCADE ON UPDATE CASCADE
             );
         ''')
 
 
 def get_tables():
-    tables = db.readTable('sqlite_master')
-    tables_to_drop = []
-    for row in tables:
-        tables_to_drop.append(row['name'])
-    return tables_to_drop
+    try:
+        tables = db.readData("SELECT name FROM sqlite_master WHERE type='table';")
+        tables_to_drop = []
+        for table in tables:
+            tables_to_drop.append(table[0])
+        return tables_to_drop
+    except:
+        return []
 
 
 def drop_tables():
-    for table in get_tables():
+    tables = get_tables()
+    for table in tables:
         if (table != 'sqlite_sequence'):
             db.executeSQL(
             f'''
                 DROP TABLE IF EXISTS {table}
-            ''')
+            ''',False)
+    
 
 
 def migrate():
